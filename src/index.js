f5f7965f41a095b32d698188f4d1c4188248616f
Karan Khosla <karan_khosla@outlook.com>
2019-03-11 10:34:39 +0530

    HOC

--- src/index.js
+++ src/index.js
@@ -14,4 +14,42 @@ const jsx = (
     </Provider>
 );
 
-ReactDOM.render(jsx, document.getElementById('root'));
+// Higher Order Component (HOC) - A component (HOC) that renders another component
+// Goal: Reuse code
+// Render hijacking
+// Prop manipulation
+// Abstract state
+
+const Info = (props) => (
+    <div>
+        <h1>Info</h1>
+        <p>The info is: {props.info}</p>
+    </div>
+);
+
+const withAdminWarning = (WrappedComponent) => {
+    return (props) => (
+        <div>
+            {props.isAdmin && <p>This is a private info. Please don't share!</p>}
+            <WrappedComponent {...props} />
+        </div>
+    );
+};
+
+const requireAuthentication = (WrappedComponent) => {
+    return (props) => (
+        <div>
+            {props.isAuthenticated ? (
+                <WrappedComponent {...props} />
+            ) : (<p>This is a private info. Please don't share!</p>)}
+        </div>
+    );
+};
+
+const AdminInfo = withAdminWarning(Info);
+const AuthInfo = requireAuthentication(Info);
+
+ReactDOM.render(<AuthInfo isAuthenticated={false} info="There are the details" />, document.getElementById('root'));
+// ReactDOM.render(<AdminInfo isAdmin={false} info="There are the details" />, document.getElementById('root'));
+// ReactDOM.render(<Info info="There are the details" />, document.getElementById('root'));
+// ReactDOM.render(jsx, document.getElementById('root'));
